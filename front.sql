c4497943ae4ef049fb752ede4c1d1e815c35c8cc
anjali2060 <118989222+anjali2060@users.noreply.github.com>
2025-08-10 17:30:07 +0530

    front.sql

---
+++ front.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employees (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100),
+  experience FLOAT,
+  education_level VARCHAR(50),
+  evaluation_score FLOAT,
+  predicted_performance VARCHAR(50)
+);
